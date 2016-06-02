f3d80eda5782e1c4bcdf49258a8e6780249882e0
gas <gasweld@gmail.com>
2016-06-03 00:17:36 +0300

    Change: 'log_message' format and default logname.

--- src/core/logs.php
+++ src/core/logs.php
@@ -2,8 +2,14 @@
 /*
  * Writes a message to the given log.
  */
-function log_message( $message, $logname ) {
-	$out = date( 'd.m.Y H:i:s' ) . "\t"	. $message.PHP_EOL;
+function log_message( $message, $logname = 'log' ) {
+	$cols = array(
+		date( 'd.m.Y H:i:s' ),
+		$_SERVER['REMOTE_ADDR'],
+		$message,
+		req_header( 'User-Agent' )
+	);
+	$out = implode( "\t", $cols ) . PHP_EOL;
 	return files::append( 'logs', "$logname.log", $out );
 }
 
