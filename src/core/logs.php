8496d7e12ba34f3806c9b303eeb9d19103fa1866
gas <gasweld@gmail.com>
2016-06-19 23:57:51 +0300

    Change: add user spec in log messages.

--- src/core/logs.php
+++ src/core/logs.php
@@ -2,11 +2,19 @@
 /*
  * Writes a message to the given log.
  */
-function log_message( $message, $logname = 'log' ) {
+function log_message( $message, $logname = 'log' )
+{
+	$id = user::get_id();
+	$type = user::get_type();
+	if( !$type ) {
+		$type = 'nobody';
+	}
+
 	$cols = array(
 		date( 'd.m.Y H:i:s' ),
-		$_SERVER['REMOTE_ADDR'],
 		$message,
+		"($type#$id)",
+		$_SERVER['REMOTE_ADDR'],
 		req_header( 'User-Agent' )
 	);
 	$out = implode( "\t", $cols ) . PHP_EOL;
