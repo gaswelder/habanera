32736c812125a6e9862e1f6f46649fcba35f4f75
gas <gasweld@gmail.com>
2016-06-20 01:09:17 +0300

    Change: buffer log messages, flush on exit.

--- src/core/logs.php
+++ src/core/logs.php
@@ -1,24 +1,10 @@
 <?php
+
 /*
  * Writes a message to the given log.
  */
-function log_message( $message, $logname = 'log' )
-{
-	$id = user::get_id();
-	$type = user::get_type();
-	if( !$type ) {
-		$type = 'nobody';
-	}
-
-	$cols = array(
-		date( 'd.m.Y H:i:s' ),
-		$message,
-		"($type#$id)",
-		$_SERVER['REMOTE_ADDR'],
-		req_header( 'User-Agent' )
-	);
-	$out = implode( "\t", $cols ) . PHP_EOL;
-	return files::append( 'logs', "$logname.log", $out );
+function log_message( $message, $logname = 'log' ) {
+	return h2_logs::log( $message, $logname );
 }
 
 /*
@@ -26,6 +12,47 @@ function log_message( $message, $logname = 'log' )
  */
 function msg( $message ) {
 	log_message( $message, 'debug' );
+}
+
+
+register_shutdown_function( 'h2_logs::flush' );
+
+class h2_logs
+{
+	private static $buf = array();
+
+	static function log( $msg, $logname = 'log' )
+	{
+		$id = user::get_id();
+		$type = user::get_type();
+		if( !$type ) {
+			$type = 'nobody';
+		}
+
+		$cols = array(
+			date( 'd.m.Y H:i:s' ),
+			$msg,
+			"($type#$id)",
+			$_SERVER['REMOTE_ADDR'],
+			req_header( 'User-Agent' )
+		);
+		$out = implode( "\t", $cols ) . PHP_EOL;
+
+		if( !isset( self::$buf[$logname] ) ) {
+			self::$buf[$logname] = $out;
+		}
+		else {
+			self::$buf[$logname] .= $out;
+		}
+	}
+
+	static function flush()
+	{
+		foreach( self::$buf as $logname => $lines ) {
+			files::append( 'logs', "$logname.log", $lines );
+		}
+		self::$buf = array();
+	}
 }
 
 ?>
