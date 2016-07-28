85686b7705009bf429ff8cb68e2a512659d7b358
gas <gasweld@gmail.com>
2016-07-28 23:40:16 +0300

    Revision

--- src/core/logs.php
+++ src/core/logs.php
@@ -14,38 +14,56 @@ function msg( $message ) {
 	log_message( $message, 'debug' );
 }
 
-
+/*
+ * Write log messages on disk at the end of the script.
+ */
 register_shutdown_function( 'h2_logs::flush' );
 
 class h2_logs
 {
+	/*
+	 * Buffer for messages.
+	 */
 	private static $buf = array();
 
 	static function log( $msg, $logname = 'log' )
 	{
-		$uid = user::type();
-		if( !$uid ) $uid = 'nobody';
-
-		$id = user::id();
-		if( $id ) $uid .= "#$id";
-
-		$uid .= '@' . $_SERVER['REMOTE_ADDR'];
-
+		/*
+		 * Compose the log line.
+		 */
 		$cols = array(
 			date( 'd.m.Y H:i:s' ),
-			$uid,
+			self::usertag(),
 			$msg,
 			current_url(),
 			req_header( 'User-Agent' )
 		);
 		$out = implode( "\t", $cols ) . PHP_EOL;
 
+		/*
+		 * Put the line to the buffer.
+		 */
 		if( !isset( self::$buf[$logname] ) ) {
 			self::$buf[$logname] = $out;
 		}
 		else {
 			self::$buf[$logname] .= $out;
 		}
+	}
+
+	/*
+	 * Returns a string tag describing the user identity.
+	 */
+	private static function usertag()
+	{
+		$tag = user::type();
+		if( !$tag ) $tag = 'nobody';
+
+		$id = user::id();
+		if( $id ) $tag .= "#$id";
+
+		$tag .= '@' . $_SERVER['REMOTE_ADDR'];
+		return $tag;
 	}
 
 	static function flush()
